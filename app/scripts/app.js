1af6d9de6b56f0621dade1546b6784ac47b59ead
Julian Rodriguez <greyfox893@mail.com>
2015-01-22 19:44:27 -0500

    Added more comments to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,14 @@
+//This variable is use to set up our routes for midification
 var timeSyncApp = angular.module('timeSyncApp', ["timer", "ngRoute", "firebase"]);
 
+//This is our main route provider.
 timeSyncApp.config(['$routeProvider', function($routeProvider){
-	
+	//this will be our landing page
 	$routeProvider.when('/', {
 		
 		templateUrl:'view/form.html',
 		controller:'timeInputs'
-	
+	//this will be our timer page where we pull infomation form the database.
 	}).when('/getTime/:id', {
 		
 		templateUrl:'view/timer.html',
